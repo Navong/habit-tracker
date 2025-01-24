7ea835b243a97173f2b421cf5c1cbaceffc36947
Navong <bongchannavong68@gmail.com>
2025-01-24 20:31:12 +0900

    fix

--- components/HabitCalendar.tsx
+++ components/HabitCalendar.tsx
@@ -52,7 +52,7 @@ const HabitCalendar = () => {
         // toast.error(errorMessage)
         console.log(isRefreshing)
       } finally {
-        setIsLoading(true)
+        setIsLoading(false)
       }
     }
     loadData()
